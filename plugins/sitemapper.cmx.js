c8f134032e4269374c4a6d10025458f94d3bee6a
ma3uk <m.gudoff@yandex.ru>
2016-07-16 18:07:59 +0500

    Update sitemapper.cmx.js

--- plugins/sitemapper.cmx.js
+++ plugins/sitemapper.cmx.js
@@ -8,12 +8,21 @@ __plugin__ = {
         "author":"Byron Lewis Kellett",
         "year":"2016"
     },
-	pages:[],
-	xml:"",
+	pages:[
+        {"title":"Sitemapper","contentData":"<button onclick='CMS.plugins.sitemapper.go()'>Sitemap</button><pre class='sitemapper-xml'></pre>"}
+    ],	xml:"",
     go:function() {
+        var pages = []
         // Sitemapper function
-		var ids = new Array();
-		var hrefs = new Array();
+		CMS.pages.forEach(function (post) {
+            pages.push("http://" + x + "/#!pages/" + post.id)
+        });
+        xmx = '<?xml version="1.0" ?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+        pages.forEach(function (page) {
+            xmx += "<url><loc>" + page + "</loc><lastmod>" + Date() + "</lastmod><changefreq>weekly</changefreq><priority>0.8</priority></url>"
+        });
+        xmx += "</urlset>";
+		document.getElementById('.sitemapper-xml').innerHTML = xmx;
 		
     }
 };
